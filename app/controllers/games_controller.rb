69443c066125af25ef7210d88e598ab661b2dca9
Szabolcs Andrasi <szabolcs@nationbuilder.com>
2014-01-23 12:21:45 -0800

    Initial Pusher integration

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,11 @@
 class GamesController < ApplicationController
 
   def index
+    Pusher['test_channel'].trigger('my_event', {
+        message: 'hello world'
+    })
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 NBShaker::Application.routes.draw do
-  #resources :games, only [:show]
+  get 'index', to: 'games#index'
+  get 'show', to: 'games#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'games#index'
+  # root 'games#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
