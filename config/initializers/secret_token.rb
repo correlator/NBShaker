b48fad0739641393549fd16206468870836c12e4
Scott Hoch <scotthoch86@gmail.com>
2014-01-23 10:21:46 -0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NBShaker::Application.config.secret_key_base = 'b866ea49881b491aa282afbeb3d1af0c2070ded2f13a15545b5c86439a30ec7644b7dce0f857a70077bf7af169c23062c910593f97d5634d2a6219fec1a9bce7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NBShaker::Application.config.session_store :cookie_store, key: '_NBShaker_session'
