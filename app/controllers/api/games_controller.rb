66c659979114fde2a9b28ac13166030b1764bf40
Szabolcs Andrasi <szabolcs@nationbuilder.com>
2014-01-24 15:24:45 -0800

    Return the active game object if there is any

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,17 +1,16 @@
 class Api::GamesController < ApplicationController
 
   def index
-    game = Game.find_by_status(:waiting)
-    unless Game.where(status: :active).empty?
-      render nothing: true
-      return
-    end
-    if game
-      game.join_2nd_player!(params[:player])
-    Pusher['nbshaker_channel'].trigger('game_status_event', message: %Q({"status": "active", "id": "#{game.id}"}))
-    else
-      game = Game.create!(player_1_name: 'Lea', status: :waiting)
-    Pusher['nbshaker_channel'].trigger('game_status_event', message: %Q({"status": "waiting", "id": "#{game.id}"}))
+    game = Game.where(status: :active).first
+    unless game
+      game = Game.find_by_status(:waiting)
+      if game
+        game.join_2nd_player!(params[:player])
+        Pusher['nbshaker_channel'].trigger('game_status_event', message: %Q({"status": "active", "id": "#{game.id}"}))
+      else
+        game = Game.create!(player_1_name: params[:player], status: :waiting)
+        Pusher['nbshaker_channel'].trigger('game_status_event', message: %Q({"status": "waiting", "id": "#{game.id}"}))
+      end
     end
 
     render json: game
@@ -23,10 +22,10 @@ class Api::GamesController < ApplicationController
       message = "Winner is: #{game.winner}"
     else
       game.update_score!("#{params[:player]}_score".to_sym, params[:score].to_i)
-      message = %Q({"id": #{game.id}, "Lea": #{game.player_1_score}, "Jim": #{game.player_2_score}})
+      message = %Q({"id": #{game.id}, "player_1": #{game.player_1_score}, "player_2": #{game.player_2_score}})
     end
     Pusher['nbshaker_channel'].trigger('score_updated_event', message: message)
-    render json: game.id
+    render json: game
   end
 
   def decay
@@ -34,7 +33,7 @@ class Api::GamesController < ApplicationController
     game = Game.where(id: params[:id].to_i, status: :active).first
     if game
       game.decay!
-      Pusher['nbshaker_channel'].trigger('score_updated_event', message: %Q({"id": #{game.id}, "Lea": #{game.player_1_score}, "Jim": #{game.player_2_score}}))
+      Pusher['nbshaker_channel'].trigger('score_updated_event', message: %Q({"id": #{game.id}, "player_1": #{game.player_1_score}, "player_2": #{game.player_2_score}}))
     end
     render nothing: true
   end
