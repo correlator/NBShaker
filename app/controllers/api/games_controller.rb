590b60e26cbacfdd5298a694eed9dc5fe42fa68c
Szabolcs Andrasi <szabolcs@nationbuilder.com>
2014-01-24 10:24:39 -0800

    Small refactoring
    - Rename some methods that modify data
    - Stop using the Pusher in the model. Use it in the controller only.

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -16,7 +16,7 @@ class Api::GamesController < ApplicationController
     if game.is_game_over?
       message = "Winner is: #{game.winner}"
     else
-      game.update_score("#{params[:player]}_score".to_sym, params[:score].to_i)
+      game.update_score!("#{params[:player]}_score".to_sym, params[:score].to_i)
       message = %Q({"id": #{game.id}, "Lea": #{game.player_1_score}, "Jim": #{game.player_2_score}})
     end
     Pusher['nbshaker_channel'].trigger('score_updated_event', message: message)
@@ -26,7 +26,10 @@ class Api::GamesController < ApplicationController
   def decay
     puts params[:id]
     game = Game.where(id: params[:id].to_i, status: :active).first
-    game.decay if game
+    if game
+      game.decay!
+      Pusher['nbshaker_channel'].trigger('score_updated_event', message: %Q({"id": #{game.id}, "Lea": #{game.player_1_score}, "Jim": #{game.player_2_score}}))
+    end
     render nothing: true
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -9,19 +9,17 @@ class Game < ActiveRecord::Base
     })
   end
 
-  def update_score(player_score, score)
+  def update_score!(player_score, score)
     old_score = self.send(player_score).to_i
     update_attribute(player_score.to_sym, old_score + score)
     update_attribute(:status, :finished) if is_game_over?
   end
 
-  def decay
+  def decay!
     update_attributes!(
       player_1_score: (player_1_score * 0.95).to_i,
       player_2_score: (player_2_score * 0.95).to_i
     )
-    Pusher['nbshaker_channel'].trigger('score_updated_event',
-                                       message: %Q({"id": #{id}, "Lea": #{player_1_score}, "Jim": #{player_2_score}}))
   end
 
   def is_game_over?
