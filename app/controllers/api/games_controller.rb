10c556e2b4aa849b51f2184cf73679f4facdd422
Szabolcs Andrasi <szabolcs@nationbuilder.com>
2014-01-24 11:12:00 -0800

    Return the game object as JSON when updating score

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -20,7 +20,7 @@ class Api::GamesController < ApplicationController
       message = %Q({"id": #{game.id}, "Lea": #{game.player_1_score}, "Jim": #{game.player_2_score}})
     end
     Pusher['nbshaker_channel'].trigger('score_updated_event', message: message)
-    render json: game.id
+    render json: game
   end
 
   def decay
